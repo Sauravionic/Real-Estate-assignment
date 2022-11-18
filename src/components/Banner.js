4e47ef6e68d7465509496b371ae6c0461608d5bc
sauravionic <srivastava2000saurav@gmail.com>
2022-11-18 21:19:49 +0530

    Added FontAwesome Dependency to the project. Closes #3

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
 //import Image
 import Image from '../assets/img/house-banner.png';
 
@@ -11,11 +12,12 @@ const Banner = () => {
     <section>
       <div className='flex justify-between px-10 items-center'>
         <div>
-          <h1>Search Properties to Rent</h1>
+          <h1 className='text-3xl'>Search Properties to Rent</h1>
         </div>
         <div>
           <form>
             <input type="search" id="" class="p-4 text-sm ring-offset-0 text-gray-900 border rounded-lg bg-white" placeholder="Search with search bar" required />
+            <FontAwesomeIcon icon = {faCircleChevronDown} ></FontAwesomeIcon>
           </form>
         </div>
       </div>
