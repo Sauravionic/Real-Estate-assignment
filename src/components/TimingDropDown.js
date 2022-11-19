eac2924b64f81d14ec0b99b5fd766a06a48c3d84
sauravionic <srivastava2000saurav@gmail.com>
2022-11-19 06:28:13 +0530

    Added the date filter. Closes #9

--- src/components/TimingDropDown.js
+++ src/components/TimingDropDown.js
@@ -1,17 +1,60 @@
-import React, { useState, useEffect, useContext } from 'react'
+import { useEffect, useRef, useState, useContext } from 'react'
+import { Calendar } from 'react-date-range'
+import format from 'date-fns/format'
 
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 // import icons
 import { RiCalendarFill, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
 
 //import headless ui
 import { Menu } from '@headlessui/react';
-
 //import House Context
 import { HouseContext } from './HouseContext';
+
+
 const TimingDropDown = () => {
   const {date, setDate} = useContext(HouseContext);
   const [isOpen, setIsOpen] = useState(false);
+  const [calendar, setCalendar] = useState('')
 
+  // open close
+  const [open, setOpen] = useState(false)
+
+  // get the target element to toggle 
+  const refOne = useRef(null)
+
+  useEffect(() => {
+    // set current date on component load
+    setCalendar(format(new Date(), 'MM/dd/yyyy'))
+    // event listeners
+    document.addEventListener("keydown", hideOnEscape, true)
+    document.addEventListener("click", hideOnClickOutside, true)
+  }, [])
+
+  // hide dropdown on ESC press
+  const hideOnEscape = (e) => {
+    // console.log(e.key)
+    if( e.key === "Escape" ) {
+      setOpen(false)
+    }
+  }
+
+  // Hide on outside click
+  const hideOnClickOutside = (e) => {
+    // console.log(refOne.current)
+    // console.log(e.target)
+    if( refOne.current && !refOne.current.contains(e.target) ) {
+      setOpen(false)
+    }
+  }
+
+  // on date change, store date in state
+  const handleSelect = (date) => {
+    // console.log(date)
+    // console.log(format(date, 'MM/dd/yyyy'))
+    setCalendar(format(date, 'MM/dd/yyyy'))
+  }
   return (
     <Menu as = 'div' className='relative'>
         <Menu.Button onClick={() => setIsOpen(!isOpen)}>
@@ -25,6 +68,27 @@ const TimingDropDown = () => {
                 )}
             </div>
         </Menu.Button>
+
+        <div className="calendarWrap">
+
+      <input
+        value={ calendar }
+        readOnly
+        className="inputBox"
+        onClick={ () => setOpen(open => !open) }
+      />
+
+      <div ref={refOne}>
+        {open && 
+          <Calendar
+            date={ new Date() }
+            onChange = { handleSelect }
+            className="calendarElement"
+          />
+        }
+      </div>
+
+    </div>
     </Menu>
   )
 }
